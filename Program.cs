dc508fcc1735b8521291329d18a42362dc6f415b
PavelKrasikov <127337558+PavelKrasikov@users.noreply.github.com>
2023-03-12 19:08:16 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число от 1 до 7: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a == 7 || a == 6)
+    Console.WriteLine ("Выходной");
+    
+else
+    Console.Write("Будний день");
