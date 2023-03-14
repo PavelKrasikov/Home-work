b600330979e17ae1d4a016e6f46af6af6a350444
PavelKrasikov <127337558+PavelKrasikov@users.noreply.github.com>
2023-03-14 14:00:28 +0300

    Delete Вторая цифра трёхзначного числа.cs

--- Вторая цифра трёхзначного числа.cs
+++
@@ -1,6 +0,0 @@
-﻿// Ввести 3ёх значное число, вывести вторую цифру этого числа.
-
-Console.WriteLine("Введите число: ");
-int a = Convert.ToInt32(Console.ReadLine());
-int b = a / 10;
-Console.WriteLine(b % 10);
