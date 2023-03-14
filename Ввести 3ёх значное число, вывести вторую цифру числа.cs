f9819d60d45227988a01a29a9e24ec53ab891a68
PavelKrasikov <127337558+PavelKrasikov@users.noreply.github.com>
2023-03-14 14:02:07 +0300

    Add files via upload

---
+++ Ввести 3ёх значное число, вывести вторую цифру числа.cs
@@ -0,0 +1,5 @@
+﻿// Ввести 3ёх значное число, вывести вторую цифру этого числа.
+
+Console.WriteLine("Enter number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine((a / 10) % 10);
