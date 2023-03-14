a365fbec55f39b1952ce4e02e683f7beeb4b8ed0
PavelKrasikov <127337558+PavelKrasikov@users.noreply.github.com>
2023-03-14 14:03:32 +0300

    Add files via upload

---
+++ Третья цифры слева.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Enter number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a < 100)
+    
+        Console.WriteLine("Third digit no");
+
+    if (a > 99 && a < 1000)
+       {
+        Console.WriteLine(a % 10);
+       }
+       if (a > 999 && a < 10000)
+          {
+           Console.WriteLine((a / 10) % 10);
+          }
+          if (a > 9999 && a < 100000)
+             {
+                Console.WriteLine((a / 100) % 10);
+             }
+             if (a > 99999 && a < 1000000)
+                {
+                    Console.WriteLine((a / 1000) % 10);
+                }
+
